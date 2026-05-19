02456c5267684b5e5220a2438fa9c90a78d3c6e2
Hussain512 <shujaatfida512@gmail.com>
2026-05-19 14:28:33 +0700

    Update Front After Removal

--- CircularQueuesArray_0227.cpp
+++ CircularQueuesArray_0227.cpp
@@ -52,6 +52,24 @@ public:
         }
 
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            if (FRONT == max - 1)
+            {
+                FRONT = 0;
+            }
+            else
+            {
+                FRONT = FRONT + 1;
+            }
+        }
+    }
 };
 
 
