62ac344dc63dbf2de8e3fc3eb7de684bc3e5f749
Hussain512 <shujaatfida512@gmail.com>
2026-05-19 14:30:27 +0700

    Check Empty Queue in Display

--- CircularQueuesArray_0227.cpp
+++ CircularQueuesArray_0227.cpp
@@ -76,6 +76,14 @@ public:
         int FRONT_Position = FRONT;
         int REAR_Position = REAR;
 
+
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
 };
 
 
