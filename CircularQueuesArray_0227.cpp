3a8406db3d17d5e9b75edf4262af554091d495e2
Hussain512 <shujaatfida512@gmail.com>
2026-05-19 14:22:23 +0700

    Update Front and Rear Positions

--- CircularQueuesArray_0227.cpp
+++ CircularQueuesArray_0227.cpp
@@ -30,6 +30,19 @@ void insert()
         return;
     }
 
+    if (FRONT == -1)
+    {
+        FRONT = 0;
+        REAR = 0;
+    }
+    else
+    {
+        if (REAR == max - 1)
+            REAR = 0;
+        else
+            REAR = REAR + 1;
+    }
+
 int main()
 {  
 }
