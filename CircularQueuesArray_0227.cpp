921c84fa6ebc809585f75eb53b83b3af3d3e167d
Hussain512 <shujaatfida512@gmail.com>
2026-05-19 14:25:30 +0700

    fixing errors

--- CircularQueuesArray_0227.cpp
+++ CircularQueuesArray_0227.cpp
@@ -15,35 +15,36 @@ public:
         FRONT = -1;
         REAR = -1;
     }
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
 };
 
-void insert()
-{
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << endl;
 
-    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
-    {
-        cout << "\nQueue overflow\n";
-        return;
-    }
-
-    if (FRONT == -1)
-    {
-        FRONT = 0;
-        REAR = 0;
-    }
-    else
-    {
-        if (REAR == max - 1)
-            REAR = 0;
-        else
-            REAR = REAR + 1;
-    }
-    queue_array[REAR] = num;
-}
 
 int main()
 {  
