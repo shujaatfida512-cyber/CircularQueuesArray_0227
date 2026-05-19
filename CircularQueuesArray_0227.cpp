fe1a9feebe5f25c34739ab07735b0d04364c9041
Hussain512 <shujaatfida512@gmail.com>
2026-05-19 14:31:02 +0700

    Display Queue Elements

--- CircularQueuesArray_0227.cpp
+++ CircularQueuesArray_0227.cpp
@@ -84,6 +84,17 @@ public:
         }
 
         cout << "\nElements in the queue are...\n";
+
+        if (FRONT_Position <= REAR_Position)
+        {
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
+    }
 };
 
 
